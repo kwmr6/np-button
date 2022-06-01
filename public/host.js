cc168969ea136be5a26441fc73c18de114693114
kwmr6 <96163480+kwmr6@users.noreply.github.com>
2022-06-01 22:26:43 +0900

    textcntent -> syusei

--- public/host.js
+++ public/host.js
@@ -13,7 +13,15 @@ socket.on('UserList', function (onlineUsers) {
         if(!li){
             li = document.createElement('li');
             li.id = u.id;
-            li.textContent = u.nickname + " " + u.realname; //できなさそうだったら（）で実名囲むのもあり
+            const span1= document.createElement("span");
+            span1.classList.add("nickname");
+            span1.textcontent =u.nickname;
+            li.appendChild(span1);
+            const span2= document.createElement("span");
+            span2.classList.add("realname");
+            span2.textcontent =u.realname;
+            li.appendChild(span2);
+           // li.textContent = u.nickname + " " + u.realname; //できなさそうだったら（）で実名囲むのもあり
         }
         li.style.display = 'inline-block';
         li.style.visibility = u.hidden ? 'hidden' : 'visible';
